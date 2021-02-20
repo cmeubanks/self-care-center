2458e73038117cb4bf7789a9d6a47ce260b17e54
Caroline Eubanks <cmeubanks@gmail.com>
2021-02-20 15:06:01 -0700

    added wrapper on message-type section so that H2 and section elements could be hidden together when form page displays

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+// User can add their own message
+// Add an “Add Message” button in a logical spot.
+//      Create button in HTML under where the messages are displaying
+
+// When the user clicks “Add Message” a form will appear in the message box. The form should contain two inputs - one to specify which type of message is being added, and another to add the text of the message itself. The form should also contain a “Submit” button.
+//      Create hidden form element in HTML -one with radio buttons to select message type (way to reuse  checkRadioBtn?)
+//      other form input is for text and then below this is a submit button
+//      make sure to add to changeView function
+
+// If the user doesn’t specify the type of message, they should get an error and be unable to submit their message until the type is specified.
+    //conditional within submit button event handler that throws error message
+    //return that displays this message and ends function?
+// When the user clicks the “Submit” button and that message will be added to the appropriate list of messages.
+  //adds message to respectice array with .push and uses .value to get user input message and reassigns to class "display-message"
+
+// When a new message is added, that message should automatically be displayed in the message box, instead of the meditation icon.
+  //hides form and displays main page again with new message
+
+// As you add these new elements to the page, be sure to match the style of existing elements.
+
+// NOTE: None of this needs to persist on page refresh, unless you also complete the local storage feature
+//research - different types of error message displays - ones builot into HTML?
 
 /* global variables */
 var savedMessages = [];
@@ -7,14 +29,13 @@ var currentMessage
 var imageView = document.querySelector('#image-view');
 var messageView = document.querySelector('#message-view');
 var displayMessage = document.querySelector('.display-message');
+
 /* button selectors */
 var radioBtn = document.querySelectorAll('input[name="radio"]');
 var receiveBtn = document.querySelector('.receive-button');
 
+/* event listeners */
 receiveBtn.addEventListener('click', showMessage);
-
-
-
 
 /*functions*/
 function changeView(view1, view2) {
@@ -24,7 +45,7 @@ view2.classList.remove('hidden');
 
 function getRandomNumber(array) {
   return Math.floor(Math.random() * Math.floor(array.length));
-}
+};
 
 function checkRadioBtn() {
   var checkedBtn
@@ -34,7 +55,7 @@ function checkRadioBtn() {
     }
   }
   return checkedBtn
-}
+};
 
 function showMessage() {
   var button = checkRadioBtn();
@@ -49,4 +70,4 @@ function showMessage() {
     return alert('Select message type!')
   }
   changeView(imageView, messageView);
-}
+};
